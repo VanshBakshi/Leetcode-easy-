dbc6fba75085b1fd5d918e6be132940995008c21
Vansh Bakshi <vanshbakshi62@gmail.com>
2026-09-24 16:19:59 +0530

    3550. Smallest Index With Digit Sum Equal to Index

---
+++ easy_05.py
@@ -0,0 +1,9 @@
+class Solution:
+    def smallestIndex(self, nums):
+        for i in range(len(nums)):
+            digit_sum = sum(int(digit) for digit in str(nums[i]))
+
+            if digit_sum == i:
+                return i
+
+        return -1
